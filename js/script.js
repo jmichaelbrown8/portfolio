1f67540319743e87879a89249a075155af9a9fc6
J. Michael Brown <jmichaelbrown8@gmail.com>
2022-03-06 14:15:50 -0800

    Added new projects

--- js/script.js
+++ js/script.js
@@ -1,39 +1,55 @@
-const workContainer = document.querySelector('#work');
+const workContainer = document.querySelector("#work");
 
 const projects = [
-    {
-        name: 'Grill: Food and Friends',
-        subtitle: 'Search for recipes to grill while grilling your friends with trivia!',
-        id: 'work-grill',
-        class: 'hero',
-        liveURL: 'https://jmichaelbrown8.github.io/grill',
-        codeURL: 'https://github.com/jmichaelbrown8/grill'
-    },
-    {
-        name: 'Wedding Site',
-        subtitle: 'A live information site I created for my wedding.',
-        id: 'work-wedding',
-        liveURL: 'https://jmichaelbrown8.github.io/weddingsite/',
-        codeURL: 'https://github.com/jmichaelbrown8/weddingsite'
-    },
-    {
-        name: 'Interest Calc',
-        subtitle: 'A simple javascript calculator for compounding interest.',
-        id: 'work-interest',
-        liveURL: 'https://jmichaelbrown8.github.io/compoundinterest/',
-        codeURL: 'https://github.com/jmichaelbrown8/compoundinterest'
-    },
-    {
-        name: 'Scrum Game',
-        subtitle: 'Test your Scrum framework knowledge.',
-        id: 'work-scrumgame',
-        liveURL: 'https://agility.help/scrumgame/',
-        codeURL: 'https://github.com/jmichaelbrown8/scrum-game'
-    },
+  {
+    name: "Agility Help",
+    subtitle: "A blog site for helpful agile software development tips.",
+    id: "work-agility-help",
+    class: "hero",
+    liveURL: "https://agility-help.herokuapp.com",
+    codeURL: "https://github.com/jmichaelbrown8/agility-help",
+  },
+  {
+    name: "Tutor Tea",
+    subtitle:
+      "Rate and review your tutor to help others in bootcamp to get paired up.",
+    id: "work-tutor-tea",
+    liveURL: "https://tutor-tea.herokuapp.com",
+    codeURL: "https://github.com/jmichaelbrown8/tutortea",
+  },
+  {
+    name: "Grill: Food and Friends",
+    subtitle:
+      "Search for recipes to grill while grilling your friends with trivia!",
+    id: "work-grill",
+    liveURL: "https://grill-n.herokuapp.com/",
+    codeURL: "https://github.com/jmichaelbrown8/grill",
+  },
+  {
+    name: "Wedding Site",
+    subtitle: "A live information site I created for my wedding.",
+    id: "work-wedding",
+    liveURL: "https://jmichaelbrown8.github.io/weddingsite/",
+    codeURL: "https://github.com/jmichaelbrown8/weddingsite",
+  },
+  {
+    name: "Interest Calc",
+    subtitle: "A simple javascript calculator for compounding interest.",
+    id: "work-interest",
+    liveURL: "https://jmichaelbrown8.github.io/compoundinterest/",
+    codeURL: "https://github.com/jmichaelbrown8/compoundinterest",
+  },
+  {
+    name: "Scrum Game",
+    subtitle: "Test your Scrum framework knowledge.",
+    id: "work-scrumgame",
+    liveURL: "https://agility.help/scrumgame/",
+    codeURL: "https://github.com/jmichaelbrown8/scrum-game",
+  },
 ];
 
 function renderWork(workObj) {
-    return `
+  return `
     <div id="${workObj.id}" class="${workObj.class}">
         <div>
             <h2>${workObj.name}</h2>
@@ -48,6 +64,6 @@ function renderWork(workObj) {
 }
 
 for (let i = 0; i < projects.length; i++) {
-    let workHTML = renderWork(projects[i]);
-    workContainer.innerHTML += workHTML;
+  let workHTML = renderWork(projects[i]);
+  workContainer.innerHTML += workHTML;
 }
