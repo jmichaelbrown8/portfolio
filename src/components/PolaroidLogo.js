360a1a36f3fd6963233f520a6221520d5d017f17
J. Michael Brown <jmichaelbrown8@gmail.com>
2022-04-02 10:11:11 -0700

    Added a polaroid logo component

---
+++ src/components/PolaroidLogo.js
@@ -0,0 +1,23 @@
+const styles = {
+  height: "0.5rem",
+  width: "9rem",
+  display: "grid",
+  margin: "0.5rem auto 0",
+  position: "relative",
+  left: "-11.5rem",
+  gridTemplateColumns: "repeat(5, 1fr)",
+};
+
+function PolaroidLogo() {
+  return (
+    <div style={styles}>
+      <div className="red"></div>
+      <div className="orange"></div>
+      <div className="yellow darken-1"></div>
+      <div className="green"></div>
+      <div className="blue"></div>
+    </div>
+  );
+}
+
+export default PolaroidLogo;
--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,3 +1,4 @@
+import PolaroidLogo from "../components/PolaroidLogo";
 import Nav from "./Nav";
 
 const styles = {
@@ -10,7 +11,11 @@ const styles = {
 function Header({ currentPage, handlePageChange }) {
   return (
     <header style={styles} className="grey darken-4">
-      <h1>J. Michael Brown</h1>
+      <h1>
+        J. Michael Brown
+        <PolaroidLogo />
+      </h1>
+
       <h5>Full Stack Software Engineer</h5>
 
       <Nav handlePageChange={handlePageChange} currentPage={currentPage} />
