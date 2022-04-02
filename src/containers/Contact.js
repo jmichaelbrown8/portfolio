32d21327571a2d8dc21d064e8b8aabba39708236
J. Michael Brown <jmichaelbrown8@gmail.com>
2022-04-02 01:57:24 -0700

    Fixed form behavior by including the materialize js files

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -7,25 +7,6 @@ const styles = {
   padding: "5em 0",
 };
 
-// Needed to get label to move out of the way of the inputs
-const onFocus = (e) => {
-  const targetId = e.target.getAttribute("id");
-  const labelEl = document.querySelector(`[for=${targetId}]`);
-  labelEl.setAttribute("class", "active");
-};
-
-// Allows the label to move back into position with no data
-const onBlur = (e) => {
-  const value = e.target.value;
-  const targetId = e.target.getAttribute("id");
-  const labelEl = document.querySelector(`[for=${targetId}]`);
-  if (value) {
-    labelEl.setAttribute("class", "active");
-  } else {
-    labelEl.removeAttribute("class");
-  }
-};
-
 function Contact() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -42,8 +23,6 @@ function Contact() {
               onChange={(e) => {
                 setName(e.target.value);
               }}
-              onFocus={onFocus}
-              onBlur={onBlur}
               className="validate"
               type="text"
             />
@@ -61,15 +40,13 @@ function Contact() {
               onChange={(e) => {
                 setEmail(e.target.value);
               }}
-              onFocus={onFocus}
-              onBlur={onBlur}
               className="validate"
               type="email"
             />
             <label htmlFor="email">Email</label>
             <span
               className="helper-text"
-              data-error="Please enter an email"
+              data-error="Please enter a valid email address"
               data-success=""
             ></span>
           </div>
@@ -83,8 +60,6 @@ function Contact() {
               onChange={(e) => {
                 setMessage(e.target.value);
               }}
-              onFocus={onFocus}
-              onBlur={onBlur}
               className="materialize-textarea"
             ></textarea>
             <label htmlFor="message" className="">
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "materialize-css/dist/css/materialize.min.css";
+import "materialize-css/dist/js/materialize.min.js";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
